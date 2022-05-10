6c84a0923f01be573795f347f4833b4ebcc0d1f7
U94702 <koen.schuykens@kbc.be>
2022-05-10 13:37:59 +0200

    commit 2

--- shopping-list/src/main/java/com/ezgroceries/shoppinglist/cocktailController.java
+++ shopping-list/src/main/java/com/ezgroceries/shoppinglist/cocktailController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.annotation.Resources;
 import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
